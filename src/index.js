1a60a2269c91eb318c76e9bc6db40d857d6a7f84
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2023-06-23 08:49:13 -0300

    feat: create components

--- src/index.js
+++ src/index.js
@@ -1,65 +1,119 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 const pizzaData = [
   {
-    name: "Focaccia",
-    ingredients: "Bread with italian olive oil and rosemary",
+    name: 'Focaccia',
+    ingredients: 'Bread with italian olive oil and rosemary',
     price: 6,
-    photoName: "pizzas/focaccia.jpg",
+    photoName: 'pizzas/focaccia.jpg',
     soldOut: false,
   },
   {
-    name: "Pizza Margherita",
-    ingredients: "Tomato and mozarella",
+    name: 'Pizza Margherita',
+    ingredients: 'Tomato and mozarella',
     price: 10,
-    photoName: "pizzas/margherita.jpg",
+    photoName: 'pizzas/margherita.jpg',
     soldOut: false,
   },
   {
-    name: "Pizza Spinaci",
-    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    name: 'Pizza Spinaci',
+    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
     price: 12,
-    photoName: "pizzas/spinaci.jpg",
+    photoName: 'pizzas/spinaci.jpg',
     soldOut: false,
   },
   {
-    name: "Pizza Funghi",
-    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    name: 'Pizza Funghi',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
     price: 12,
-    photoName: "pizzas/funghi.jpg",
+    photoName: 'pizzas/funghi.jpg',
     soldOut: false,
   },
   {
-    name: "Pizza Salamino",
-    ingredients: "Tomato, mozarella, and pepperoni",
+    name: 'Pizza Salamino',
+    ingredients: 'Tomato, mozarella, and pepperoni',
     price: 15,
-    photoName: "pizzas/salamino.jpg",
+    photoName: 'pizzas/salamino.jpg',
     soldOut: true,
   },
   {
-    name: "Pizza Prosciutto",
-    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    name: 'Pizza Prosciutto',
+    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
     price: 18,
-    photoName: "pizzas/prosciutto.jpg",
+    photoName: 'pizzas/prosciutto.jpg',
     soldOut: false,
   },
 ];
 
-
 function App() {
-  return <>
-  <h1>Pizza Menu</h1>
-  <Pizza />
-  </>;
+  return (
+    <div className='container'>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
 }
 
-function Pizza() {
-  return <>
-  <img src='pizzas/spinaci.jpg' alt='Pizza Salamino' />
-  <h1 >Pizza Salamino </h1>
-  <p>Tomato, mozarella, and pepperoni</p>
-  </>
+function Header() {
+  return (
+    <header className='header'>
+      <h1>Feast React Pizza Go.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className='menu'>
+      <h2>Our menu</h2>
+      <Pizza
+        name='Pizzaa Spinaci'
+        ingredients='Tomato, mozarella, and pepperoni'
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+    </main>
+  );
+}
+
+function Pizza(props) {
+  const { name, ingredients, photoName, price } = props;
+  return (
+    <div className='pizza'>
+      <img src={ photoName } alt={ name } />
+      <div>
+        <h1>{ name }</h1>
+        <p>{ ingredients }</p>
+        <span>{ price }</span>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  const workingHours = {
+    currentHour: new Date().getHours(),
+    openHour: 12,
+    closeHour: 22,
+    closeMessage: "We're currently open!",
+    openMessage: "Sorry we're closed",
+    checkOpeningStatus() {
+      const status =
+        this.openHour >= this.currentHour && this.openHour <= this.currentHour
+          ? this.openMessage
+          : this.closeMessage;
+      return status;
+    },
+  };
+  return (
+    <footer className='footer'>
+      {new Date().toLocaleTimeString()}. {workingHours.checkOpeningStatus()}
+    </footer>
+  );
+  // return React.createElement('footer', null, "We're currently open!");
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
